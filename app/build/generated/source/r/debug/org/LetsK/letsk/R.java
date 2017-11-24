670e1a6e2a250b6f8c975834c7737fad7cdc133c
Hazel <hazelsuko@gmail.com>
2017-11-24 14:55:10 +0800

    basic change for UI
    + color
    + 2 imagebuttons

--- app/build/generated/source/r/debug/org/LetsK/letsk/R.java
+++ app/build/generated/source/r/debug/org/LetsK/letsk/R.java
@@ -14,11 +14,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
+        public static final int blue=0x7f020003;
+        public static final int darkgray=0x7f020004;
         public static final int ic_launcher=0x7f020000;
+        public static final int lightgray=0x7f020005;
+        public static final int lightred=0x7f020006;
+        public static final int playbutton=0x7f020001;
+        public static final int recordbutton=0x7f020002;
+        public static final int themedark=0x7f020007;
+        public static final int themelight=0x7f020008;
+        public static final int white=0x7f020009;
     }
     public static final class id {
-        public static final int play=0x7f060005;
-        public static final int progressbar=0x7f060003;
+        public static final int play=0x7f060003;
+        public static final int progressbar=0x7f060005;
         public static final int record=0x7f060004;
         public static final int tv=0x7f060002;
         public static final int tvpitch=0x7f060000;
